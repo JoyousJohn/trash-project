abb8956e631f4d24eaee7529bf2cd491f435f164
John <john.happy@rutgers.edu>
2023-03-21 02:56:04 -0400

    Revert "Revert "Pythonic-ification"" and uglification
    This reverts commit 2193e8222abb6f16c158887f7d008ab80cdbfd48.

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
-import cherrypy
+import cherrypy, os
 from mako.template import Template
-import os
 
 #app.config['TEMPLATES_AUTO_RELOAD'] = True # Flask
 
 class index(object):
+
     @cherrypy.expose
     def index(self):
-        template = Template(filename='templates/index.html')
-        return template.render()
+        
+        return Template(filename='templates/index.html').render()
 
 if __name__ == '__main__':
 
     cherrypy.config.update({
 
         'server.socket_host': '0.0.0.0',
-        'server.socket_port': int(os.getenv("PORT", 8080)),
+        'server.socket_port': int(os.getenv('PORT', 8080)),
         
     })
 
     PATH = os.path.abspath(os.path.dirname(__file__))
 
-    cherrypy.quickstart(index(), "/", {
+    cherrypy.quickstart(index(), '/', {
         
-        "/css": {
-            "tools.staticdir.on": True,
-            "tools.staticdir.dir": f"{PATH}/static/css/"
+        '/css': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': f'{PATH}/static/css/'
         }, 
 
-        "/img": {
-            "tools.staticdir.on": True,
-            "tools.staticdir.dir": f"{PATH}/static/img/"
+        '/img': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': f'{PATH}/static/img/'
         }
     })
 
-    #app.run(port=os.getenv("PORT", default=5000), host=os.getenv("HOST")) #FLASK
-    #serve(app, host=os.getenv("HOST", default='127.0.0.1'), port=os.getenv("PORT", default=5000)) #UVICORN WAITRESS
+    #app.run(port=os.getenv('PORT', default=5000), host=os.getenv('HOST')) #FLASK
+    #serve(app, host=os.getenv('HOST', default='127.0.0.1'), port=os.getenv('PORT', default=5000)) #UVICORN WAITRESS
 
